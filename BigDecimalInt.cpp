720b907aa4a0f0fc7d40ae652917e80da7f4f328
NourMuhammad10 <nourmuhammad835@gmail.com>
2022-10-15 21:31:44 +0200

    Edits to constructors

--- BigDecimalInt.cpp
+++ BigDecimalInt.cpp
@@ -6,7 +6,7 @@ char a =' ';
 int numberOfDigits;
 
 BigDecimalInt::BigDecimalInt(int decInt) {
-    int index;
+    int index=0;
     if( decInt<0){
         decInt*=-1;
         Sign =true;
@@ -20,26 +20,29 @@ BigDecimalInt::BigDecimalInt(int decInt) {
 
     }
     while(decInt!=0){
-        num[index]=decInt%10;
+        num[index]='decInt%10';
         decInt = decInt/10;
         index--;
     }
-
+cout<<num<<endl;
 }
-BigDecimalInt::BigDecimalInt(string decStr){
-    if(decStr[1]=='+' || decStr[1] =='-'){
-        cout<<"This input doesn't represent a valid format for the number\nPlease enter the number with only one sign\n";
-    }
-    else if(decStr.find(a) != string::npos){
-        cout<<"This input doesn't represent a valid format for the number\nPlease enter the number without spaces\n";
-    }
-    else {
-        num = decStr;
-        if(decStr[0]=='-'){
-            Sign= true;
-        }
-        else{
+BigDecimalInt::BigDecimalInt(string decStr) {
+    if (decStr[1] == '+' || decStr[1] == '-') {
+        cout<< "This input doesn't represent a valid format for the number\nPlease enter the number with only one sign\n";
+    } else if (decStr.find(a) != string::npos) {
+        cout << "This input doesn't represent a valid format for the number\nPlease enter the number without spaces\n";
+    } else {
+        if (decStr[0] == '-') {
+            Sign = true;
+            for (int i = 1; i < decStr.size(); i++) {
+                num.push_back(decStr[i]);
+            }
+        } else {
             Sign = false;
+            for (int i = 1; i < decStr.size(); i++) {
+                num.push_back(decStr[i]);
+            }
         }
+        cout << num << endl;
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 #include "BigDecimalInt.h"
+#include<cmath>
 using namespace std;
 
 int main(){
-
+BigDecimalInt num1 ("-12345678945372863838262828");
+BigDecimalInt(12436);
+int number= 5273;
+cout<<int(log10(number) + 1)<<endl;
 }
